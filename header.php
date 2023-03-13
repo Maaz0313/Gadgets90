1676ff4f18b2bb4de1985e1527f6c0146d9bf0d8
Maaz0313 <maaztajammul63@gmail.com>
2023-03-13 16:18:40 +0500

    not modified

--- header.php
+++ header.php
@@ -1,6 +1,5 @@
 <!doctype html>
 <html>
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -9,7 +8,6 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"/>
     <title>Gadgets90</title>
 </head>
-
 <body>
     <nav class="relative px-5 flex justify-between items-center h-14 bg-black drop-shadow-md">
         <!-- Logo -->
