7e6a48f0df3702008606231654dec737b4bd2366
Maaz0313 <maaztajammul63@gmail.com>
2023-03-22 00:06:07 +0500

    Created specs page using grid-col

---
+++ specs.php
@@ -0,0 +1,26 @@
+<?php
+require('header.php');
+?>
+
+        <div class="grid grid-cols-4 gap-3 my-6 mx-6">
+            <div><img class="w-16 md:w-32 lg:w-36" src="http://localhost/Gadgets90/img/poco-x5-5g.PNG" alt=""></div>
+            <div class="col-span-3 grid grid-cols-3 gap-3">
+                <div class="col-span-3"><h1 class="text-2xl font-bold">Poco X5 5G</h1></div>
+                <div class="col-span-3"><p>Poco X5 5G best price in India is at ₹18,999. The lowest price of Poco X5 5G is ₹18,999 in Flipkart.com.</p>
+                </div>
+                <div class="col-span-3"><p class="text-xl font-medium">Key Specifications</p></div>
+                <div style="background-color:#f6f6f4" class="col-span-3 grid grid-cols-3 gap-2">
+                <div><p class="font-medium">Launch Date</p><br>March 21, 2023</div>
+                <div><p class="font-medium">Front Camera</p><br>March 21, 2023</div>
+                <div><p class="font-medium">Processor</p><br>March 21, 2023</div>
+                <div><p class="font-medium">RAM</p><br>March 21, 2023</div>
+                <div><p class="font-medium">Rear Camera</p><br>March 21, 2023</div>
+                <div><p class="font-medium">Battery</p><br>March 21, 2023</div>
+                <div><p class="font-medium">Storage</p><br>March 21, 2023</div>
+                <div><p class="font-medium">Operating System</p><br>March 21, 2023</div>
+                <div><p class="font-medium">Display</p><br>March 21, 2023</div>
+                </div>
+            </div>
+        </div>
+     
+<?php require('footer.php'); ?>
