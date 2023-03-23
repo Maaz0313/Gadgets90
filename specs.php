c5b52e007826b16a8ecac8888771fa5868304604
Maaz0313 <maaztajammul63@gmail.com>
2023-03-23 13:09:39 +0500

    M

--- specs.php
+++ specs.php
@@ -2,25 +2,28 @@
 require('header.php');
 ?>
 
-        <div class="grid grid-cols-4 gap-3 my-6 mx-6">
-            <div><img class="w-16 md:w-32 lg:w-36" src="http://localhost/Gadgets90/img/poco-x5-5g.PNG" alt=""></div>
-            <div class="col-span-3 grid grid-cols-3 gap-3">
-                <div class="col-span-3"><h1 class="text-2xl font-bold">Poco X5 5G</h1></div>
-                <div class="col-span-3"><p>Poco X5 5G best price in India is at ₹18,999. The lowest price of Poco X5 5G is ₹18,999 in Flipkart.com.</p>
-                </div>
-                <div class="col-span-3"><p class="text-xl font-medium">Key Specifications</p></div>
-                <div style="background-color:#f6f6f4" class="col-span-3 grid grid-cols-3 gap-2">
-                <div><p class="font-medium">Launch Date</p><br>March 21, 2023</div>
-                <div><p class="font-medium">Front Camera</p><br>March 21, 2023</div>
-                <div><p class="font-medium">Processor</p><br>March 21, 2023</div>
-                <div><p class="font-medium">RAM</p><br>March 21, 2023</div>
-                <div><p class="font-medium">Rear Camera</p><br>March 21, 2023</div>
-                <div><p class="font-medium">Battery</p><br>March 21, 2023</div>
-                <div><p class="font-medium">Storage</p><br>March 21, 2023</div>
-                <div><p class="font-medium">Operating System</p><br>March 21, 2023</div>
-                <div><p class="font-medium">Display</p><br>March 21, 2023</div>
-                </div>
+    <div class="grid grid-cols-4 gap-3 my-6 mx-6">
+        <div><img class="w-16 md:w-32 lg:w-36" src="http://localhost/Gadgets90/img/poco-x5-5g.PNG" alt=""></div>
+        <div class="col-span-3 grid grid-cols-3 gap-3">
+            <div class="col-span-3"><h1 class="text-2xl font-bold">Poco X5 5G</h1></div>
+            <div class="col-span-3"><p>Poco X5 5G best price in India is at ₹18,999. The lowest price of Poco X5 5G is ₹18,999 in Flipkart.com.</p>
+            </div>
+            <div class="col-span-3"><p class="text-xl font-medium">Key Specifications</p></div>
+            <div style="background-color:#f6f6f4" class="col-span-3 grid grid-cols-3 gap-2">
+            <div><p class="font-medium">Launch Date</p><br>March 21, 2023</div>
+            <div><p class="font-medium">Front Camera</p><br>March 21, 2023</div>
+            <div><p class="font-medium">Processor</p><br>March 21, 2023</div>
+            <div><p class="font-medium">RAM</p><br>March 21, 2023</div>
+            <div><p class="font-medium">Rear Camera</p><br>March 21, 2023</div>
+            <div><p class="font-medium">Battery</p><br>March 21, 2023</div>
+            <div><p class="font-medium">Storage</p><br>March 21, 2023</div>
+            <div><p class="font-medium">Operating System</p><br>March 21, 2023</div>
+            <div><p class="font-medium">Display</p><br>March 21, 2023</div>
             </div>
         </div>
-     
+    </div>
+    <div class="my-6 mx-6 pt-2 border-t-2 border-black">
+    <b>Xiaomi Poco X5 GT - The Latest Model From The X5 Series!</b> <br>
+Xiaomi is launching the new Poco X5 series with a brand new GT model that will be available soon in markets. It might be the high-end model of this series, according to leaks and news. The new GT model is rumoured to include features like a larger AMOLED display, improved camera capabilities, a powerful processor, and a larger battery capacity. These features make it stand out from its predecessors and make it an ideal choice for those looking for a high-end smartphone. Let's reveal the amazing features of Xiaomi's Poco X5 GT, featuring a larger 6.67-inch HD+ OLED Capacitive Touchscreen display with a 120Hz refresh rate and 1080 x 2400 pixel resolution. The Xiaomi Poco X5's a well-known Adreno 618 GPU that will allow users to load graphics in high resolution without any errors. A further advantage of this device is the fact that it comes with 12GB of RAM and 512GB of internal storage. Poco X5 GT is fitted with a powerful and well-known Qualcomm Snapdragon 7 Gen 2 chipset, as well as an Octa Core CPU. There is no doubt that the couple of these processors will be able to handle multiple large applications without any problems. Xiaomi Poco's X5 GT has got a triple camera set up with the main camera being 50 MP, and two others being 8 MP and 2 MP. These massive sensors will be able to enhance the quality of photography on this smartphone. On the front, it will have a single 16 MP selfie camera. As part of the Xiaomi X5 GT, the battery has been increased to 5500mAh, so that it can last all day on just one charge, and it is able to support fast charging of 67W so that it can be fully charged within minutes. Samsung and other brands are going to face a new challenger in the form of the Poco X5 GT by Xiaomi.
+    </div>
 <?php require('footer.php'); ?>
