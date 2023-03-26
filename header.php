4542991bb1af829f0ff374161ff6cbf4aa9c87f7
Maaz0313 <maaztajammul63@gmail.com>
2023-03-26 13:10:45 +0500

    testing navbars

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="https://cdn.jsdelivr.net/npm/tailwindcss/dist/tailwind.min.css" rel="stylesheet">
+    <!-- <link href="https://cdn.jsdelivr.net/npm/tailwindcss/dist/tailwind.min.css" rel="stylesheet"> -->
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"/>
     <title>Gadgets90</title>
@@ -47,6 +47,84 @@
                 id="toggle-menu-icon" class="fa-bars fa-solid"></i></a>
 
     </nav>
+<nav class="bg-gray-800">
+  <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+    <div class="flex justify-between h-16">
+      <!-- Left items -->
+      <div class="flex-shrink-0 flex items-center">
+        <a href="#" class="text-white font-bold">Logo</a>
+      </div>
+
+      <!-- Center items -->
+      <div class="flex-grow flex items-center justify-center">
+        <a href="#" class="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Item 1</a>
+        <a href="#" class="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Item 2</a>
+        <a href="#" class="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Item 3</a>
+      </div>
+
+      <!-- Right items -->
+      <div class="flex items-center">
+        <a href="#" class="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Login</a>
+        <a href="#" class="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Signup</a>
+      </div>
+    </div>
+  </div>
+</nav>
+<nav class="bg-gray-800">
+  <div class="mx-auto px-4 sm:px-6 lg:px-8">
+    <div class="flex items-center justify-between h-16">
+      <div class="flex items-center">
+        <!-- Left items -->
+        <div class="flex-shrink-0">
+          <a href="#" class="text-white font-bold">Logo</a>
+        </div>
+        <div class="hidden md:block">
+          <ul class="ml-10 flex items-baseline space-x-4">
+            <li>
+              <a href="#" class="text-gray-300 hover:text-white">Home</a>
+            </li>
+            <li>
+              <a href="#" class="text-gray-300 hover:text-white">About</a>
+            </li>
+            <li>
+              <a href="#" class="text-gray-300 hover:text-white">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <!-- Center items -->
+      <div class="hidden md:block">
+        <div class="ml-4 flex items-center md:ml-6">
+          <button class="bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
+            <span class="sr-only">View notifications</span>
+            <!-- Notification icon -->
+          </button>
+        </div>
+      </div>
+      <div class="-mr-2 flex md:hidden">
+        <!-- Hamburger button -->
+        <button type="button" class="bg-gray-800 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
+          <span class="sr-only">Open main menu</span>
+          <!-- Hamburger icon -->
+        </button>
+      </div>
+      <!-- Right items -->
+      <div class="hidden md:block">
+        <div class="ml-4 flex items-center md:ml-6">
+          <a href="#" class="text-gray-300 hover:text-white">Login</a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- Mobile menu, show/hide based on menu state. -->
+  <div class="md:hidden" id="mobile-menu">
+    <div class="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+      <a href="#" class="text-gray-300 hover:text-white block">Home</a>
+      <a href="#" class="text-gray-300 hover:text-white block">About</a>
+      <a href="#" class="text-gray-300 hover:text-white block">Contact</a>
+    </div>
+  </div>
+</nav>
 
     <div class="container text-2xl items-center px-5 py-24 mx-auto flex flex-wrap" style="background:lightgrey">
       Your ad here
