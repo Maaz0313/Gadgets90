a9651a7b8ad245124ceb5a018e24e954fa607dd4
Maaz0313 <maaztajammul63@gmail.com>
2023-03-29 11:52:06 +0500

    M

--- mobile specs.php
+++ mobile specs.php
@@ -3,7 +3,7 @@ require('header.php');
 ?>
 
 <div style="width: 100%;">
-        <div class="grid grid-cols-4 gap-3 my-6 mx-6">
+        <div class="grid grid-cols-4 gap-3 my-6 mx-36">
         <div><img class="w-16 md:w-32 lg:w-36" src="http://localhost/Gadgets90/img/poco-x5-5g.PNG" alt=""></div>
         <div class="col-span-3 grid grid-cols-3 gap-3">
             <div class="col-span-3"><h1 class="text-2xl font-bold">Poco X5 5G</h1></div>
@@ -80,13 +80,13 @@ require('header.php');
             </div>
         </div>
     </div>
-    <div class="my-6 mx-6 pt-2 border-t-2 border-black">
+    <div class="my-6 mx-36 pt-2 border-t-2 border-black">
     <b>Xiaomi Poco X5 GT - The Latest Model From The X5 Series!</b> <br>
 Xiaomi is launching the new Poco X5 series with a brand new GT model that will be available soon in markets. It might be the high-end model of this series, according to leaks and news. The new GT model is rumoured to include features like a larger AMOLED display, improved camera capabilities, a powerful processor, and a larger battery capacity. These features make it stand out from its predecessors and make it an ideal choice for those looking for a high-end smartphone. Let's reveal the amazing features of Xiaomi's Poco X5 GT, featuring a larger 6.67-inch HD+ OLED Capacitive Touchscreen display with a 120Hz refresh rate and 1080 x 2400 pixel resolution. The Xiaomi Poco X5's a well-known Adreno 618 GPU that will allow users to load graphics in high resolution without any errors. A further advantage of this device is the fact that it comes with 12GB of RAM and 512GB of internal storage. Poco X5 GT is fitted with a powerful and well-known Qualcomm Snapdragon 7 Gen 2 chipset, as well as an Octa Core CPU. There is no doubt that the couple of these processors will be able to handle multiple large applications without any problems. Xiaomi Poco's X5 GT has got a triple camera set up with the main camera being 50 MP, and two others being 8 MP and 2 MP. These massive sensors will be able to enhance the quality of photography on this smartphone. On the front, it will have a single 16 MP selfie camera. As part of the Xiaomi X5 GT, the battery has been increased to 5500mAh, so that it can last all day on just one charge, and it is able to support fast charging of 67W so that it can be fully charged within minutes. Samsung and other brands are going to face a new challenger in the form of the Poco X5 GT by Xiaomi.
     </div>
-    <div class="my-6 mx-6">
-        <h2 class="text-lg font-bold">Poco X5 5G Full Specifications</h2>
-        <table class="table-auto border-solid border-2 border-black">
+    <div class="my-6 mx-36">
+        <h2 class="text-lg font-bold">Poco X5 5G Full Specifications</h2><br>
+        <table class="table-auto mx-auto border-solid border-2 border-black">
             <tr>
                 <th align="left" class="border-solid border-2 border-black py-2 px-2" rowspan="6">Build</TH>
                 <th align="left"  class="border-solid border-2 border-black py-2 px-2">OS</th>
