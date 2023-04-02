b123173c0a814107c2d6292c0778a093213f8080
Maaz0313 <maaztajammul63@gmail.com>
2023-04-02 12:13:08 +0500

    created specs pages

--- headsets.php
+++ headsets.php
@@ -730,7 +730,7 @@ require('header.php');
         <div class="lg:col-span-3" style="margin-top:0.5rem;">
           <ul class="grid gap-4 sm:grid-cols-2 lg:grid-cols-3">
             <li>
-              <a href="#" class="group block overflow-hidden">
+              <a href="headset specs.php" class="group block overflow-hidden">
                 <img
                   src="https://images.unsplash.com/photo-1523381210434-271e8be1f52b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
                   alt=""
@@ -754,7 +754,7 @@ require('header.php');
             </li>
   
             <li>
-              <a href="#" class="group block overflow-hidden">
+              <a href="headset specs.php" class="group block overflow-hidden">
                 <img
                   src="https://images.unsplash.com/photo-1523381210434-271e8be1f52b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
                   alt=""
@@ -778,7 +778,7 @@ require('header.php');
             </li>
   
             <li>
-              <a href="#" class="group block overflow-hidden">
+              <a href="headset specs.php" class="group block overflow-hidden">
                 <img
                   src="https://images.unsplash.com/photo-1523381210434-271e8be1f52b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
                   alt=""
@@ -800,34 +800,8 @@ require('header.php');
                 </div>
               </a>
             </li>
-          </ul>
-          <ul class="grid gap-4 sm:grid-cols-2 lg:grid-cols-3">
             <li>
-              <a href="#" class="group block overflow-hidden">
-                <img
-                  src="https://images.unsplash.com/photo-1523381210434-271e8be1f52b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
-                  alt=""
-                  class="h-[200px] w-full object-cover transition duration-500 group-hover:scale-105 sm:h-[200px]"
-                />
-  
-                <div class="relative bg-white pt-3">
-                  <h3
-                    class="text-xs text-gray-700 group-hover:underline group-hover:underline-offset-4"
-                  >
-                    Basic Tee
-                  </h3>
-  
-                  <p class="mt-2">
-                    <span class="sr-only"> Regular Price </span>
-  
-                    <span class="tracking-wider text-gray-900"> £24.00 GBP </span>
-                  </p>
-                </div>
-              </a>
-            </li>
-  
-            <li>
-              <a href="#" class="group block overflow-hidden">
+              <a href="headset specs.php" class="group block overflow-hidden">
                 <img
                   src="https://images.unsplash.com/photo-1523381210434-271e8be1f52b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
                   alt=""
@@ -851,7 +825,7 @@ require('header.php');
             </li>
   
             <li>
-              <a href="#" class="group block overflow-hidden">
+              <a href="headset specs.php" class="group block overflow-hidden">
                 <img
                   src="https://images.unsplash.com/photo-1523381210434-271e8be1f52b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
                   alt=""
@@ -873,58 +847,8 @@ require('header.php');
                 </div>
               </a>
             </li>
-          </ul>
-          <ul class="grid gap-4 sm:grid-cols-2 lg:grid-cols-3">
             <li>
-              <a href="#" class="group block overflow-hidden">
-                <img
-                  src="https://images.unsplash.com/photo-1523381210434-271e8be1f52b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
-                  alt=""
-                  class="h-[200px] w-full object-cover transition duration-500 group-hover:scale-105 sm:h-[200px]"
-                />
-  
-                <div class="relative bg-white pt-3">
-                  <h3
-                    class="text-xs text-gray-700 group-hover:underline group-hover:underline-offset-4"
-                  >
-                    Basic Tee
-                  </h3>
-  
-                  <p class="mt-2">
-                    <span class="sr-only"> Regular Price </span>
-  
-                    <span class="tracking-wider text-gray-900"> £24.00 GBP </span>
-                  </p>
-                </div>
-              </a>
-            </li>
-  
-            <li>
-              <a href="#" class="group block overflow-hidden">
-                <img
-                  src="https://images.unsplash.com/photo-1523381210434-271e8be1f52b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
-                  alt=""
-                  class="h-[200px] w-full object-cover transition duration-500 group-hover:scale-105 sm:h-[200px]"
-                />
-  
-                <div class="relative bg-white pt-3">
-                  <h3
-                    class="text-xs text-gray-700 group-hover:underline group-hover:underline-offset-4"
-                  >
-                    Basic Tee
-                  </h3>
-  
-                  <p class="mt-2">
-                    <span class="sr-only"> Regular Price </span>
-  
-                    <span class="tracking-wider text-gray-900"> £24.00 GBP </span>
-                  </p>
-                </div>
-              </a>
-            </li>
-  
-            <li>
-              <a href="#" class="group block overflow-hidden">
+              <a href="headset specs.php" class="group block overflow-hidden">
                 <img
                   src="https://images.unsplash.com/photo-1523381210434-271e8be1f52b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
                   alt=""
