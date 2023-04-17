7b715f4e60917b2b68ac41f2f92f74489f717bb9
Maaz0313 <maaztajammul63@gmail.com>
2023-04-17 11:34:35 +0500

    completed

--- user/login.php
+++ user/login.php
@@ -16,11 +16,11 @@ require('../header.php');
 					<form class="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
 						<div class="relative">
 							<input autocomplete="new-email" id="email" name="email" type="text" class="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Email address" />
-							<label for="email" class="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Email Address</label>
+							<label for="email" class="absolute left-0 -top-3.5 text-gray-600 text-sm">Email Address</label>
 						</div>
 						<div class="relative">
 							<input autocomplete="new-password" id="password" name="password" type="password" class="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Password" />
-							<label for="password" class="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Password</label>
+							<label for="password" class="absolute left-0 -top-3.5 text-gray-600 text-sm">Password</label>
 						</div>
 						<div class="relative">
 							<button class="bg-blue-500 text-white rounded-md px-3 py-2">Log In</button>
