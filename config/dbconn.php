ad17bdb3b21495c3587ab434d17cec34b0055bbd
Maaz0313 <maaztajammul63@gmail.com>
2023-12-16 00:09:25 +0500

    Started creating admin panel

---
+++ config/dbconn.php
@@ -0,0 +1,13 @@
+<?php
+$hostname = "localhost";
+$username = "root";
+$password = "";
+$database = "gadgets90";
+$conn = mysqli_connect($hostname, $username, $password, $database);
+
+if (!$conn) {
+    die("Connection Failed: " . mysqli_connect_error());
+}
+else{
+    echo "Connection successful!";
+}
